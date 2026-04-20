7f79af2a26a72597728e74275098c6963ab65dcd
sato.hiroki <sato.hiroki@no1s.biz>
2026-04-21 01:35:51 +0900

    Revert "callback test"
    This reverts commit cd6ea4a61be5a474ad7f6d415fc99fd968697d4e.

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const express = require('express')
 const app = express()
-const port = "nothing, it's callback function."// 3000
+const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// app.listen(port, () => {
-// ポート未指定では、空いているポートが自動選択される
-const server = app.listen(() => {
-  const { port, address, family } = server.address();
-  console.log(`listening on adress: ${address}, port: ${port} (family: ${family})`);
-});
-
-// ちなみにexpressはnodeのhttpサーバの関数を呼び出しているだけらしい
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
